7882bf153d7b5b377c3ab78199fe4c1407ca9718
StardustGarden <h81nguyen@protonmail.com>
2021-11-25 13:22:46 +0700

    Revert "Latest source code dump from November 2020"
    This reverts commit 99e2872c5679bd235fe23e73b103e783064f44e7.

--- src/process/process.cpp
+++
@@ -1,9 +0,0 @@
-// Licensed under GPLv2 or later until the library is deemed stable enough for general use, see LICENSE in the source tree.
-#include <process/process.hpp>
-
-namespace lavender {
-
-namespace process {
-}
-
-}
